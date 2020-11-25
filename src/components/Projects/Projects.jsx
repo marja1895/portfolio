6c2f35f7c965c448e8f1867f48833b73e147d952
Alice <marja1895@gmail.com>
2020-11-25 12:05:41 +0200

    editing Projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -41,10 +41,11 @@ const Projects = () => {
                     distance="30px"
                   >
                     <div className="project-wrapper__text">
-                      <h3 className="project-wrapper__text-title">{title}</h3>
+                      <h3 className="project-wrapper__text-title">{title || ''}</h3>
                       <div>
                         <p>
-                          {info}
+                          {info ||
+                            ''}
                         </p>
                         <p className="mb-4">{info2 || ''}</p>
                       </div>
